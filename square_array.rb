4751a1aab78ecfeb81982b4034f2a771a60faa28
Robert Snyder <r.snyder49@gmail.com>
2018-08-07 23:00:41 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -8,7 +8,7 @@ def square_array(array)
 end
 
 def square_array(array)
-  array.collect do |num|
-    num**2 #ruby exponent operator = x ** y where y is the exponent
+  array.collect do |num| # collect returns a new array, each returns the original array
+    num**2 # ruby exponent operator = x ** y where y is the exponent
   end 
 end
