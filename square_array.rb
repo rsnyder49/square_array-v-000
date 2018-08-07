d8d75b3f6814329a488bbf56e5b6ca169ee4a693
Robert Snyder <r.snyder49@gmail.com>
2018-08-07 22:52:33 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   new_array = []
   array.each do |number|
-    squared_num = number * number
+    squared_num = number * number #ruby exponent
     new_array.push(squared_num)
   end
   return new_array
